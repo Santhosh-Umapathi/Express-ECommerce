03b612da2b0df3e0dedf54f5da6564d1d2db25e5
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2021-11-30 22:54:49 +0100

    FEAT: Pre methods added to Schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator").default;
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const COLLECTION_NAME = "users";
 
@@ -36,5 +39,48 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+//Runs before saving - Middleware for Mongoose
+userSchema.pre("save", async function (next) {
+  //Exit if already hashed
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //Hash only on first time/forgot password
+  this.password = await bcryptjs.hash(this.password, 10);
+});
+
+//Validate the password with user password
+userSchema.methods.isValidPassword = async function (userPassword) {
+  return await bcryptjs.compare(userPassword, this.password);
+};
+
+//Create JWT
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRY,
+    }
+  );
+};
+
+// Generate Forgot Password Token
+userSchema.methods.getForgotPasswordToken = function () {
+  const forgotToken = crypto.randomBytes(20).toString("hex"); //Random String, sent to user
+
+  // Generating advanced hash - Stored only in db to compare later
+  this.forgotPasswordToken = crypto
+    .createHash("sha256")
+    .update(forgotToken)
+    .digest("hex");
+
+  this.forgotPasswordExpiry = Date.now() * 20 * 60 * 1000; // 20 mins
+
+  return forgotToken;
+};
 
 module.exports = mongoose.model(COLLECTION_NAME, userSchema);
