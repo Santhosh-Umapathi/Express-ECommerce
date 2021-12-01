0b1d6d9b1e097eefba4da9567bd6ffdb4aab8b9d
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2021-12-01 06:32:09 +0100

    FEAT: DB connection added

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDb = () =>
+  mongoose
+    .connect(process.env.MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected"))
+    .catch((e) => {
+      console.log("DB Connection failed", e);
+      process.exit(1);
+    });
+
+module.exports = { connectDb };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const app = require("./app");
+const { connectDb } = require("./config");
+
+//Connect Database
+connectDb();
 
 app.listen(process.env.PORT, () =>
   console.log("Listening to PORT:", process.env.PORT)
