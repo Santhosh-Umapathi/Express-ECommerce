56ea4b28f11a8dedc59ae60acb8d7bac24ee404e
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2021-12-05 14:23:51 +0100

    FEAT: Changes updated

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,9 +1,12 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const Razorpay = require("razorpay");
 
 //Middlewares
 const { BigPromise } = require("../middlewares");
 //Error
 const { CustomError } = require("../error");
+
+//Stripe
 //Just sending the publishable key to Client, Publish key is safe to share
 const sendStripeKey = BigPromise(async (req, res, next) => {
   res
@@ -28,4 +31,35 @@ const captureStripePayment = BigPromise(async (req, res, next) => {
   });
 });
 
-module.exports = { sendStripeKey, captureStripePayment };
+//Razorpay
+const sendRazorpayKey = BigPromise(async (req, res, next) => {
+  res.status(200).json({
+    razorpaykey: process.env.RAZORPAY_API_KEY,
+  });
+});
+
+const captureRazorpayPayment = BigPromise(async (req, res, next) => {
+  var instance = new Razorpay({
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_SECRET,
+  });
+
+  var options = {
+    amount: req.body.amount, // amount in the smallest currency unit
+    currency: "INR",
+  };
+  const myOrder = await instance.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    amount: req.body.amount,
+    order: myOrder,
+  });
+});
+
+module.exports = {
+  sendStripeKey,
+  captureStripePayment,
+  sendRazorpayKey,
+  captureRazorpayPayment,
+};
